9398ef4885af84d0f0dfa08aa0455ab55038bd38
RyanD <rdo9313@gmail.com>
2024-02-08 01:34:58 -0500

    data types exercises

---
+++ data_types/ex_1.py
@@ -0,0 +1,12 @@
+"""
+'True'    => String
+False     => Boolean
+(1, 2, 3) => Tuple
+1.5       => Float
+[1, 2, 3] => List
+2         => Integer
+range(5)  => Range
+{1, 2, 3} => Set
+None      => NoneType
+{'foo': 'bar' } => Dictionary
+"""
