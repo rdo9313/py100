af7357bc0dc21ea01e12d588c08043e81f99624a
RyanD <rdo9313@gmail.com>
2024-02-12 12:07:59 -0500

    variables as pointers exercises

---
+++ var_as_pointers/ex_1.py
@@ -0,0 +1,5 @@
+my_object1 == my_object2
+my_object1 is my_object2
+
+# Line 1 checks whether the values of both objects are the same while
+# Line 2 checks whether the objects the variables point to are the same (ids are equal)
